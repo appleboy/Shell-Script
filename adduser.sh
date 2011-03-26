605ddde8c532d2066e3e424168a28d3daf39e065
Bo-Yi Wu <appleboy.tw@gmail.com>
2011-03-26 10:23:12 +0800

    transfer tab to space

--- adduser.sh
+++ adduser.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ################################################################################
-# Date:		2011/03/14
+# Date:     2011/03/14
 # Author:   appleboy ( appleboy.tw AT gmail.com)
 # Web:      http://blog.wu-boy.com
 #
 # Program:
-#	Use function to add system user and samba permission.
+#   Use the function to add system user and samba permission.
 #
 # History:
-#	2011/03/14 (first release)
+#   2011/03/14 (first release)
 #   2011/03/15 (add default_group default_home default_shell for config)
 #   2011/03/25 (rewrite process command line)
 #
@@ -37,8 +37,8 @@ function displayErr()
 
 function usage()
 {
-	echo 'Usage: '$0' --action [add|del] Username'
-	exit 1;
+    echo 'Usage: '$0' --action [add|del] Username'
+    exit 1;
 }
 
 execute ()
@@ -64,28 +64,28 @@ test -z $action && usage $0
 test -z $username && usage $0
 
 case $action in
-	"add")
-		# check if username exist
-		username_exist=$(awk -F ":" '{printf $1 "\n"}' /etc/passwd | grep "^${username}$")
+    "add")
+        # check if username exist
+        username_exist=$(awk -F ":" '{printf $1 "\n"}' /etc/passwd | grep "^${username}$")
 
-		if [ ! -z ${username_exist} ] ; then
-		    displayErr "Username $username exist, please change it"
-		fi
+        if [ ! -z ${username_exist} ] ; then
+            displayErr "Username $username exist, please change it"
+        fi
 
-		# add user
-		password=$(echo $username | mkpasswd -s)
-		cmd=$(useradd -c "$username" -g $default_group -d "${default_home}/$username" --password $password -m -s $default_shell $username)
+        # generate password and add user
+        password=$(echo $username | mkpasswd -s)
+        cmd=$(useradd -c "$username" -g $default_group -d "${default_home}/$username" --password $password -m -s $default_shell $username)
 
-		# add samba user
-		[ $samba_enable -ne "0" ] && ((echo $username; echo $username) | smbpasswd -L -s -a $username > /dev/null && smbpasswd -L -s -e $username > /dev/null && echo "add samba user ${username}")
-		[ -z $cmd ] && echo "add username $username successfully";
+        # add samba user
+        [ $samba_enable -ne "0" ] && ((echo $username; echo $username) | smbpasswd -L -s -a $username > /dev/null && smbpasswd -L -s -e $username > /dev/null && echo "add samba user ${username}")
+        [ -z $cmd ] && echo "add username $username successfully";
 
-		;;
-	"del")
-		[ $samba_enable -ne "0" ] && (smbpasswd -x $username > /dev/null && echo "delete samba user ${username}")
-		userdel -r $username && echo "delete user ${username} and remove home directory"
-		;;
-	*)
-		usage $0
-		;;
+        ;;
+    "del")
+        [ $samba_enable -ne "0" ] && (smbpasswd -x $username > /dev/null && echo "delete samba user ${username}")
+        userdel -r $username && echo "delete user ${username} and remove home directory"
+        ;;
+    *)
+        usage $0
+        ;;
 esac
