459a14ac00b5d6489aa63b14c2da210fa2edb69c
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-07-19 19:42:02 +0800

    add comment header.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ create_multiple_user/adduser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##########################################################
+# Date:     2013/07/19
+# Author:   appleboy (appleboy.tw AT gmail.com)
+# Web:      http://blog.wu-boy.com
+#
+# Program:
+#   Add multiple user account via file.
+#
+# History:
+#   2013/07/19 first release
+#
+##########################################################
