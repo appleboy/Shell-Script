99a7f7b68713f9e666b4846752f9c926192c995c
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-07-20 18:15:39 +0800

    add shell command
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- create_multiple_user/adduser.sh
+++ create_multiple_user/adduser.sh
@@ -13,6 +13,47 @@
 #
 #################################################
 
+VERSION="0.1"
+ROOT_UID=0
+
+#
+# main function
+#
+
+function displayErr() {
+    echo
+    echo $1;
+    echo
+    exit 1;
+}
+
+function usage() {
+    echo 'Usage: '$0' [-h|-a|-d] config_file_path'
+    exit 1;
+}
+
+execute () {
+    $* >/dev/null
+    if [ $? -ne 0 ]; then
+        displayErr "ERROR: executing $*"
+    fi
+}
+
+if [ "$UID" -ne "$ROOT_UID" ]; then
+    displayErr "Must be root to run this script."
+fi
+
+# Process command line...
+while [ $# -gt 0 ]; do
+    case $1 in
+        -h)
+            usage $0
+        ;;
+        --action | -a) shift; action=$1; shift; username=$1; shift; password=$1; shift; ;;
+        *) usage $0; ;;
+    esac
+done
+
 cat config.txt | while read USER PASSWORD
 do
     echo "Create user: ${USER}"
