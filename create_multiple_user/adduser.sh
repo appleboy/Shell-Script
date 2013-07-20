b2e35a0996e0c07dcc073a958ed2f11cf4b5dce5
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-07-20 11:24:27 +0800

    add user by creat_user command
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- create_multiple_user/adduser.sh
+++ create_multiple_user/adduser.sh
@@ -5,9 +5,21 @@
 # Web:      http://blog.wu-boy.com
 #
 # Program:
-#   Add multiple user account via file.
+#   Add multiple user account via config file.
 #
 # History:
 #   2013/07/19 first release
 #
 #################################################
+
+cat config.txt | while read USER PASSWORD
+do
+    echo "Create user: ${USER}"
+    create_user --action add ${USER} ${PASSWORD}
+done
+
+cat config.txt | while read USER PASSWORD
+do
+    echo "Delete user: ${USER}"
+    create_user --action del ${USER} ${PASSWORD}
+done
