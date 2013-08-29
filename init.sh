81b6416478584fe3531decbd3a48d671a9cb8435
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-08-29 11:49:11 +0800

    remove source command.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- init.sh
+++ init.sh
@@ -6,7 +6,6 @@ aptitude -y install make git
 # fetch dotfiles and script
 cd ~ && git clone https://github.com/appleboy/dotfiles.git
 cd ~/dotfiles && make linux
-source ~/.bashrc
 cd ~ && git clone https://github.com/appleboy/Shell-Script.git
 
 echo
