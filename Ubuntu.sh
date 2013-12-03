7d65528b03b895b29d96f6e672f045b7d5fe0518
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-12-03 10:15:50 +0800

    add s4cmd and s3cp S3 command line tool
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -15,7 +15,7 @@ server_name=`lsb_release -ds | awk -F ' ' '{printf $1}' | tr A-Z a-z`
 version_name=`lsb_release -cs`
 
 usage() {
-    echo 'Usage: '$0' [--help|-h] [-i|--install] [timezone|jenkins|mosh|gearman|nginx|nginx-spdy|percona|mariadb|clean-kernel|server|desktop|initial|all]'
+    echo 'Usage: '$0' [--help|-h] [-i|--install] [s4cmd|timezone|jenkins|mosh|gearman|nginx|nginx-spdy|percona|mariadb|clean-kernel|server|desktop|initial|all]'
     exit 1;
 }
 
@@ -179,6 +179,11 @@ install_timezone() {
     hwclock -w
 }
 
+install_s4cmd() {
+    cd ~ && git clone https://github.com/bloomreach/s4cmd.git
+    cp -r s4cmd/s4cmd.py /usr/bin/
+}
+
 server() {
     output "Install Server Packages."
 
@@ -278,6 +283,13 @@ server() {
     # Optimize images using multiple utilities
     # ref: https://github.com/toy/image_optim
     gem install image_optim
+
+    # S3CP: Commands-line tools for Amazon S3 file manipulation
+    # ref: https://github.com/aboisvert/s3cp
+    gem install s3cp
+    # usage:
+    # export AWS_ACCESS_KEY_ID=xxxx
+    # export AWS_SECRET_ACCESS_KEY=xxx
 
     # install PPA purge command
     aptitude -y install ppa-purge
@@ -384,6 +396,11 @@ server() {
 
     # https://github.com/seb-m/pyinotify
     pip install pyinotify
+
+    # install Amazone Super S3 command line tool
+    # ref: https://github.com/bloomreach/s4cmd
+    pip install boto
+    install_s4cmd
 
     # remove MTA service
     aptitude -y remove exim4
@@ -535,6 +552,9 @@ case $action in
         ;;
     "timezone")
         install_timezone
+        ;;
+    "s4cmd")
+        install_s4cmd
         ;;
     "all")
         initial
