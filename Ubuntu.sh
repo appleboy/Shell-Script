c7ed7d0ca8a9348a5038dd26f0e37e62ef178486
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-01-29 10:21:58 +0800

    install ruby environment

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -170,6 +170,16 @@ install_proftpd() {
     aptitude -y install proftpd
 }
 
+install_ruby() {
+    aptitude -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
+    \curl -sSL https://get.rvm.io | bash -s stable --ruby
+    echo 'source /usr/local/rvm/scripts/rvm' >> ~/.bashrc
+    . ~/.bashrc
+    # Don’t require rdoc and ri when installing gems
+    echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+    gem i bundler rails
+}
+
 install_timezone() {
     # update time zone
     cp -r /usr/share/zoneinfo/Asia/Taipei /etc/localtime
