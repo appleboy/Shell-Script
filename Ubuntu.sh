b27488041afe52ef61a143fb4940ce05ed4578fe
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-10-02 16:19:27 +0800

    remove nginx spdy section.

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -15,7 +15,7 @@ server_name=`lsb_release -ds | awk -F ' ' '{printf $1}' | tr A-Z a-z`
 version_name=`lsb_release -cs`
 
 usage() {
-    echo 'Usage: '$0' [--help|-h] [-i|--install] [git-extras|postgresql|hhvm|elasticsearch|ajenti|redis|ruby|perl|s4cmd|optipng|timezone|jenkins|mosh|gearman|nginx|nginx-spdy|percona|mariadb|clean-kernel|server|desktop|initial|all]'
+    echo 'Usage: '$0' [--help|-h] [-i|--install] [git-extras|postgresql|hhvm|elasticsearch|ajenti|redis|ruby|perl|s4cmd|optipng|timezone|jenkins|mosh|gearman|nginx|percona|mariadb|clean-kernel|server|desktop|initial|all]'
     exit 1;
 }
 
@@ -185,51 +185,6 @@ install_nginx() {
     fi
     aptitude -y update
     aptitude -y install nginx
-}
-
-install_nginx_spdy() {
-    output "Install Nginx SPDY server."
-    # install dependence package.
-    aptitude -y install libpcre3-dev libgd-dev libgd2-xpm-dev libgeoip-dev libxslt-dev
-    # install nginx 1.4.x up version with spdy module
-    [ -f /tmp/nginx-1.6.1.tar.gz ] || wget http://nginx.org/download/nginx-1.6.1.tar.gz -O /tmp/nginx-1.6.1.tar.gz
-    # download openssl library
-    [ -f /tmp/openssl-1.0.1g.tar.gz ] || wget http://www.openssl.org/source/openssl-1.0.1g.tar.gz -O /tmp/openssl-1.0.1g.tar.gz
-    [ -d /tmp/nginx-1.6.1 ] && cd /tmp/nginx-1.6.1 && make clean
-    [ -d /tmp/openssl-1.0.1g ] && rm -rf /tmp/openssl-1.0.1g
-    [ -d /tmp/nginx-1.6.1 ] || tar -zxvf /tmp/nginx-1.6.1.tar.gz -C /tmp
-    [ -d /tmp/openssl-1.0.1g ] || tar -zxvf /tmp/openssl-1.0.1g.tar.gz -C /tmp
-    # generate makefile
-    cd /tmp/nginx-1.6.1 && ./configure \
-        --prefix=/usr/share/nginx \
-        --sbin-path=/usr/sbin/nginx \
-        --conf-path=/etc/nginx/nginx.conf \
-        --error-log-path=/var/log/nginx/error.log \
-        --http-log-path=/var/log/nginx/access.log \
-        --pid-path=/var/run/nginx.pid \
-        --user=nginx \
-        --group=nginx \
-        --with-http_realip_module \
-        --with-http_addition_module \
-        --with-http_xslt_module \
-        --with-http_image_filter_module \
-        --with-http_geoip_module \
-        --with-http_sub_module \
-        --with-http_dav_module \
-        --with-http_flv_module \
-        --with-http_mp4_module \
-        --with-http_gzip_static_module \
-        --with-http_random_index_module \
-        --with-http_secure_link_module \
-        --with-http_degradation_module \
-        --with-http_stub_status_module \
-        --with-http_perl_module \
-        --with-mail \
-        --with-mail_ssl_module \
-        --with-http_ssl_module \
-        --with-http_spdy_module \
-        --with-openssl=/tmp/openssl-1.0.1g
-    cd /tmp/nginx-1.6.1 && make && make install
 }
 
 install_gearmand() {
@@ -636,9 +591,6 @@ case $action in
         ;;
     "percona")
         install_percona
-        ;;
-    "nginx-spdy")
-        install_nginx_spdy
         ;;
     "nginx")
         install_nginx
