c999e35baf73eed36891c808eaace887247d149e
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-12-10 15:37:56 +0800

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -55,7 +55,7 @@ install_postgresql() {
 }
 
 install_docker() {
-    # https://docs.docker.com/engine/installation/debianapt-key
+    # https://docs.docker.com/engine/installation/debian
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     aptitude -y update
     aptitude -y install docker-engine
