39cc67e9ec5d3c4e4e13496fc468e5eafa190678
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-07-23 11:59:24 +0800

    Install docker compose

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -59,6 +59,11 @@ install_docker() {
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     aptitude -y update
     aptitude -y install docker-engine
+    # install docker compose
+    # https://docs.docker.com/compose/install/
+    curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
     # or install from single command
     # wget -qO- https://get.docker.com/ | sh
     # Error message Depends: init-system-helpers (>= 1.13~) but 1.7 is installed.
