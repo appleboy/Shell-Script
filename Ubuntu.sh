bc5c6c997efb95fb43255b5e85cc3dde7bcdd1cb
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-10-22 15:54:43 +0800

    soft link tmux to /usr/bin

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -74,6 +74,7 @@ install_tmux() {
   apt-get -y install make g++ gawk automake pkg-config libevent-dev ncurses-dev
   git clone https://github.com/tmux/tmux.git
   cd tmux/ && sh autogen.sh && ./configure && make && make install
+  ln -sf /usr/local/bin/tmux /usr/bin/tmux
 }
 
 install_hhvm() {
