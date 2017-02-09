07ceb7600b5165fe655b268e8d19c933cb04e1fa
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-09 10:37:14 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-alias brc='source bootstrap.sh'
-
 # install some command
 which aptitude || apt-get -qqy install aptitude
 aptitude -y install make git
 
 # fetch dotfiles and script
 cd ~ && git clone https://github.com/appleboy/dotfiles.git
-cd ~/dotfiles && brc
+cd ~/dotfiles && /bin/bash -c "source bootstrap.sh"
 cd ~ && git clone https://github.com/appleboy/Shell-Script.git
 
 echo
