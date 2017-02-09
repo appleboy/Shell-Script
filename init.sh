4b6e37a352cbe2d5798b9e936ce6c04c203a3ecc
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-09 10:26:04 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ aptitude -y install make git
 
 # fetch dotfiles and script
 cd ~ && git clone https://github.com/appleboy/dotfiles.git
-cd ~/dotfiles && source bootstrap.sh
+cd ~/dotfiles && . bootstrap.sh
 cd ~ && git clone https://github.com/appleboy/Shell-Script.git
 
 echo
