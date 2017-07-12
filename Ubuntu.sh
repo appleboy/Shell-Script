418e7df931ac4593a0530fe8d366fc6eb7763313
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-07-11 22:34:53 -0500

    update docker-compose version (#22)

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -82,7 +82,7 @@ install_docker() {
 
     # install docker compose
     # https://docs.docker.com/compose/install/
-    curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     docker-compose --version
     # or install from single command
